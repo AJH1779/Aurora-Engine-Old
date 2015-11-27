fdaa0d09dbe3147ce6a4689430105efc5a20411b
Arthur <Arthur@10.34.22.138>
2015-11-27 04:32:34 +0000

    Properties file added. Temporary file that requires proper meshing with Mod functionality.

---
+++ Aurora Engine Core/src/com/ajh1779/data/Properties.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ajh1779.data;
+
+import com.ajh1779.debug.AuroraLogs;
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Arthur
+ */
+public final class Properties {
+    private static final Logger LOG = AuroraLogs.getLogger(Properties.class);
+    public static final String AURORA_CORE_VERSION = "InDev V0.1.0";
+    public static final File APPDATA = getAppDataFile();
+    private static File getAppDataFile() {
+        String temp = System.getProperty("os.name").toLowerCase();
+        File file;
+        if (temp.contains("win")){
+            temp = System.getenv("APPDATA");
+            if(temp == null){temp = System.getProperty("user.home", ".");}
+            file = new File(temp);
+        } else if (temp.contains("mac")){
+            file = new File(System.getProperty("user.home","."),
+                    "Library/Application Support/");
+        } else if (temp.contains("nix") || temp.contains("sunos")){
+            file = new File(System.getProperty("user.home","."));
+        } else {
+            file = new File(System.getProperty("user.home","."));
+        }
+        if (!file.exists() && !file.mkdirs()){
+            throw new RuntimeException(
+                    "The following directory could not be created: " + file);
+        }
+        LOG.log(Level.INFO, "Default Directory is {0}", file.getAbsolutePath());
+        return file;
+    }
+    
+    public Properties(String prog_name, String prog_version) {
+        this.prog_name = prog_name;
+        this.prog_version = prog_version;
+        this.prog_dir = new File(APPDATA, "." + prog_name);
+        this.spec_dir = new File(prog_dir, "prog_version");
+    }
+    private final String prog_name, prog_version;
+    private final File prog_dir, spec_dir;
+    
+    /**
+     * Returns the name of the program.
+     * @return 
+     */
+    public String getProgramName() {
+        return prog_name;
+    }
+    /**
+     * Returns the version of the program.
+     * @return 
+     */
+    public String getProgramVersion() {
+        return prog_version;
+    }
+    
+    /**
+     * Returns the directory for the program.
+     * @return 
+     */
+    public File getProgramDirectory() {
+        return prog_dir;
+    }
+    /**
+     * Returns the specific version directory for the program.
+     * @return 
+     */
+    public File getVersionedDirectory() {
+        return spec_dir;
+    }
+}
