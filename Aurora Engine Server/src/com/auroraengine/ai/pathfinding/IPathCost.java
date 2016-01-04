7a72ace6bec4fa1b3ff242c5ffd148361e68cdea
AJH1779 <ajh1779@gmail.com>
2016-01-04 19:24:09 +0000

    Renaming the packages to something more appropriate.

--- Aurora Engine Server/src/com/auroraengine/ai/pathfinding/IPathCost.java
+++ Aurora Engine Server/src/com/auroraengine/ai/pathfinding/IPathCost.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.ajh1779.ai.pathfinding;
+package com.auroraengine.ai.pathfinding;
 
 /**
  *
--- Aurora Engine Server/src/com/auroraengine/ai/pathfinding/IPathFinder.java
+++ Aurora Engine Server/src/com/auroraengine/ai/pathfinding/IPathFinder.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.ajh1779.ai.pathfinding;
+package com.auroraengine.ai.pathfinding;
 
 import java.util.List;
 
@@ -11,8 +11,6 @@ import java.util.List;
  *
  * @author Arthur
  */
-public interface IPathNode<T, K extends IPathCost> {
-    public List<IPathNode<T, K>> getNeighbours();
-    public K getCostToMoveTo(T t, IPathNode n);
-    public K getProjectedCostTo(T t, IPathNode n);
+public interface IPathFinder<T, K extends IPathCost> {
+    public List<IPathNode<T, K>> getPath(T taker, IPathNode<T, K> start, IPathNode<T, K> end);
 }
